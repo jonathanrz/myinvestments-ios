c521b4178b89563fd220d534d4291387b46d61c3
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-07-25 22:47:56 -0300

    Making first server request

--- MyInvestments-ios/ViewController.swift
+++ MyInvestments-ios/ViewController.swift
@@ -7,10 +7,41 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyPlistManager
+
+func getEnvironmentVar(_ name: String) -> String? {
+	guard let rawValue = getenv(name) else { return nil }
+	return String(utf8String: rawValue)
+}
 
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		SwiftyPlistManager.shared.start(plistNames: ["keys"], logging: true)
+		guard
+			let authToken = SwiftyPlistManager.shared.fetchValue(for: "auth-token", fromPlistWithName: "keys"),
+			let serverUrl = SwiftyPlistManager.shared.fetchValue(for: "server-url", fromPlistWithName: "keys")
+			else { return }
+		
+		let headers: HTTPHeaders = [
+			"auth-token": authToken as! String,
+			"Accept": "application/json"
+		]
+		
+		Alamofire.request(serverUrl as! String, headers: headers)
+			.responseJSON { response in
+				print(response)
+			}
+			.responseString { response in
+				if let error = response.result.error {
+					print(error)
+				}
+				if let value = response.result.value {
+					print(value)
+				}
+			}
 	}
 }
--- Podfile
+++ Podfile
@@ -4,4 +4,5 @@ use_frameworks!
 
 target 'MyInvestments-ios' do
 	pod 'Alamofire', '~> 4.4'
+	pod 'SwiftyPlistManager'
 end
