1d72485db22df3808ab4d4b6e120bc1a02ee4e8d
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-07-29 19:57:30 -0300

    Show investment data

--- MyInvestments-ios/InvestmentViewController.swift
+++ MyInvestments-ios/InvestmentViewController.swift
@@ -2,14 +2,36 @@ import UIKit
 
 class InvestmentViewController: UIViewController {
 	@IBOutlet weak var nameLabel: UILabel!
+	@IBOutlet weak var typeLabel: UILabel!
+	@IBOutlet weak var dateLabel: UILabel!
+	@IBOutlet weak var holderImage: UIImageView!
 	
+	let dateFormatter = DateFormatter()
 	var investment: Investment?
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
+		dateFormatter.dateFormat = "dd/MM/yyyy"
+		
 		if let investment = investment {
 			nameLabel.text = investment.name
+			nameLabel.text = investment.name
+			typeLabel.text = investment.type
+			if let dueDate = investment.dueDate {
+				dateLabel.text = dateFormatter.string(from: dueDate)
+			} else {
+				dateLabel.text = ""
+			}
+			
+			switch investment.holder {
+			case "Bradesco":
+				holderImage.image = #imageLiteral(resourceName: "bradesco")
+			case "Easynvest":
+				holderImage.image = #imageLiteral(resourceName: "easynvest")
+			default:
+				holderImage.image = #imageLiteral(resourceName: "unknown")
+			}
 		}
 	}
 }
