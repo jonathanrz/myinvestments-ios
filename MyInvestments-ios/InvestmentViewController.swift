a1ab834d7fa14db6ba718a2ef829c3b25f8e1d5a
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-07-30 10:44:44 -0300

    Show income and investment data in chart

--- MyInvestments-ios/InvestmentViewController.swift
+++ MyInvestments-ios/InvestmentViewController.swift
@@ -1,12 +1,13 @@
 import UIKit
 import Charts
 
-class InvestmentViewController: UIViewController {
+class InvestmentViewController: UIViewController, IAxisValueFormatter, IValueFormatter {
 	@IBOutlet weak var nameLabel: UILabel!
 	@IBOutlet weak var typeLabel: UILabel!
 	@IBOutlet weak var dateLabel: UILabel!
 	@IBOutlet weak var holderImage: UIImageView!
 	@IBOutlet weak var incomesValueChart: LineChartView!
+	@IBOutlet weak var investmentValueChart: LineChartView!
 	
 	var server: Server? = nil
 	let dateFormatter = DateFormatter()
@@ -47,6 +48,7 @@ class InvestmentViewController: UIViewController {
 				if let incomes = incomes {
 					print(incomes)
 					self.populateIncomeValueChart(incomes: incomes)
+					self.populateInvestmentValueChart(incomes: incomes)
 				} else {
 					print(error!)
 				}
@@ -60,12 +62,53 @@ class InvestmentViewController: UIViewController {
 		var dataEntries: [ChartDataEntry] = []
 		
 		let incomesSorted = incomes.sorted(by: { $0.date.compare($1.date) == ComparisonResult.orderedAscending })
-		for (index, income) in incomesSorted.enumerated() {
-			dataEntries.append(ChartDataEntry(x: Double(index), y: income.value))
+		for income in incomesSorted {
+			dataEntries.append(ChartDataEntry(x: income.date.timeIntervalSince1970, y: income.value))
 		}
 		
 		let chartDataSet = LineChartDataSet(values: dataEntries, label: "Income value")
 		let chartData = LineChartData(dataSet: chartDataSet)
+		chartData.setValueFormatter(self)
 		incomesValueChart.data = chartData
+		incomesValueChart.xAxis.valueFormatter = self
+		incomesValueChart.leftAxis.valueFormatter = self
+		incomesValueChart.rightAxis.valueFormatter = self
+		incomesValueChart.chartDescription?.text = "Income by month"
+	}
+	
+	private func populateInvestmentValueChart(incomes: [Income]) {
+		var dataEntries: [ChartDataEntry] = []
+		
+		let incomesSorted = incomes.sorted(by: { $0.date.compare($1.date) == ComparisonResult.orderedAscending })
+		for income in incomesSorted {
+			dataEntries.append(ChartDataEntry(x: income.date.timeIntervalSince1970, y: income.value / income.quantity))
+		}
+		
+		let chartDataSet = LineChartDataSet(values: dataEntries, label: "Investment value")
+		let chartData = LineChartData(dataSet: chartDataSet)
+		chartData.setValueFormatter(self)
+		investmentValueChart.data = chartData
+		investmentValueChart.xAxis.valueFormatter = self
+		investmentValueChart.leftAxis.valueFormatter = self
+		investmentValueChart.rightAxis.valueFormatter = self
+		investmentValueChart.chartDescription?.text = "Investment value by month"
+	}
+	
+	func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+		switch axis {
+		case is XAxis:
+			let dateFormatter = DateFormatter()
+			dateFormatter.dateFormat = "MM/YY"
+			return dateFormatter.string(from: Date(timeIntervalSince1970: value))
+		case is YAxis:
+			return "R$\(value)"
+		default:
+			print("unknown axis")
+			return ""
+		}
+	}
+	
+	func stringForValue(_ value: Double, entry: ChartDataEntry, dataSetIndex: Int, viewPortHandler: ViewPortHandler?) -> String {
+		return "R$\(String(format:"%.2f", value))"
 	}
 }
